64af6ecd6cc17dfe263255889f2c12ee619f8975
xcg <2116069171@qq.com>
2023-09-25 01:20:24 +0800

    提交

---
+++ my-project/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="头像" src="./assets/img.jpg" width="300" height="300">
+  <h3>姓名:肖晨桂</h3>
+  <h3>学号:2310273039</h3>
+</template>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
